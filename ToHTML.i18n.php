02bbb97ad7fa929de9c3a33965cfa6d6cf04d10a
Mathias Ertl <mati@fsinf.at>
2008-11-10 17:16:11 +0000

    whatever

--- ToHTML.i18n.php
+++ ToHTML.i18n.php
@@ -5,12 +5,16 @@ $messages = array(
 		'noParamGiven' => 'Error: No Parameter given.',
 		'parNotExists' => 'Error: $1 doesn\'t exist.',
 		'pageHeader'=> 'HTML-Version of [[$1]]:',
+		'specialpages-specialpagegroup-tohtml' => 'vowi',
+		'specialpages-group-vowi' => 'VoWi-Extensions',
 	),
 	'de' => array(
 		'tohtml' => 'ToHTML',
 		'noParamGiven' => 'Fehler: Kein Parameter angegeben.',
 		'parNotExists' => 'Fehler: Der Artikel \'$1\' existiert nicht',
 		'pageHeader'=> 'HTML-Version von [[$1]]:',
+		'specialpages-specialpagegroup-tohtml' => 'vowi',
+		'specialpages-group-vowi' => 'VoWi-Extensions',
 	),
 );
 ?>
