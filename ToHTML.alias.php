89c18f3ae13c27edecc5a764f92cdd416710dc59
Mathias Ertl <mati@fsinf.at>
2013-05-26 15:39:29 +0200

    add alias fle

---
+++ ToHTML.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for myextension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Mathias Ertl
+ */
+$specialPageAliases['en'] = array(
+    'ToHTML' => array('ToHTML', 'ToHTML'),
+);
+
+/** Deutsch
+ * @author <your username>
+ */
+$specialPageAliases['de'] = array(
+    'ToHTML' => array('ToHTML', 'ToHTML'),
+);
